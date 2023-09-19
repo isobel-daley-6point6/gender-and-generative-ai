778ee76024bb5b3eaf8772070707024281398d14
isobel.daley <isobel.daley@6point6.co.uk>
2023-09-19 15:32:29 +0100

    moving list of products to separate file

---
+++ data/products/products.py
@@ -0,0 +1,4 @@
+products = ['beer','chocolate','ice cream','protein powder','a weight loss programme','a lawnmower','a car','a diy store', 
+'a supermarket','a clothes shop','furniture polish','a washing machine','dishwasher tablets','a vacuum cleaner',
+'candles','bubble bath','curtains','electric drills','nappies','a science museum','an art gallery',
+'a bookshop','a games console','a social network','a yoga class','a weightlifting class','a golf club','therapy']
